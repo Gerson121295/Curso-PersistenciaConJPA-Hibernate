e09c1dd41329c18d4ec7c77d798369045a26e5c0
Gerson121295 <Gerson21millones@gmail.com>
2023-08-15 18:51:12 -0600

    04 - Ciclo de vida de una entidad - Estados Insert en la entidad.

--- src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
+++ src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
@@ -16,21 +16,28 @@ public class RegistroDeProducto {
 	
 	public static void main(String[] args) {
 		Categoria celulares = new Categoria("CELULARES");
-		Producto celular = new Producto("Samsung", "telefono usado", new BigDecimal("1000"), celulares); //como el tipo del precio era BigDecimal se debio importar para escribir el precio
+		
+		//Producto celular = new Producto("Samsung", "telefono usado", new BigDecimal("1000"), celulares); //como el tipo del precio era BigDecimal se debio importar para escribir el precio
 			 
 		EntityManager em = JPAUtils.getEntityManager();	 //Metodo getEntityManager creado en JPAUtils, Mantener la conexion de los DAOS para evitar la duplicacion de codigo, ya que ellos lo pueden utilizar.
 		
-		ProductoDao productoDao = new ProductoDao(em); //se le pasa el parametro em que es el EntityManager
-		CategoriaDao categoriaDao = new CategoriaDao(em);
+		//ProductoDao productoDao = new ProductoDao(em); //se le pasa el parametro em que es el EntityManager
+		//CategoriaDao categoriaDao = new CategoriaDao(em);
 				
 		em.getTransaction().begin(); //indicarle para que deben iniciar las transacciones
 		
-		categoriaDao.guardar(celulares);//Guarda la instancia celulares que son las categorias antes que el producto ya que el producto esta relacionado con categoria.
-		productoDao.guardar(celular); //Realizar la 1ra. Persistencia. Guarda el producto utilizando ProductoDao
+		//categoriaDao.guardar(celulares);//Guarda la instancia celulares que son las categorias antes que el producto ya que el producto esta relacionado con categoria.
+		//productoDao.guardar(celular); //Realizar la 1ra. Persistencia. Guarda el producto utilizando ProductoDao
+		
+		em.persist(celulares);
+		celulares.setNombre("CUADERNOS");
 		
 		em.getTransaction().commit(); //obtenemos la transaccion y realizamos un commit, El commit envia los valores que fueron configurados, los envia a la BD
-		em.close();
+		em.close();//cerrando la transaccion
 		
+		
+		celulares.setNombre("SOFTWARE"); //esta no es considerada en los registros. esta fuera.
+	
 	}
 
 }
